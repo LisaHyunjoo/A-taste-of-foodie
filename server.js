b7592e1729216423a9fce59d40804c18e309792d
Lisa <lisahongofficial@gmail.com>
2022-08-22 15:18:51 -0400

    initial route setup

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-initial setup
+const express = require('express')
+const app = express()
+
+// index
+app.get('/foodie/restaurant', (req, res) => {
+    res.render('index.ejs')
+})
+
+// new restaurant
+app.get('/foodie/restaurant/new', (req, res) => {
+    res.render('newRestaurant.ejs')
+})
+
+// new cuisine
+app.get('/foodie/restaurant/:id/new', (req,res)=>{
+    res.render('NewCuisine.ejs')
+})
+
+// show 
+app.get('/foodie/restaurant/:id', (req,res)=>{
+    res.render('show.ejs')
+})
+
+
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000')
+})
