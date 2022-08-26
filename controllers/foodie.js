71ed16f03d1fd130b76b9b429f72e62a61e80662
Lisa <lisahongofficial@gmail.com>
2022-08-25 21:16:14 -0400

    set up controller

---
+++ controllers/foodie.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const router = express.Router()
+const Foodie = require('../models/foodie.js')
+
+// index (list of restaurants)
+router.get('', (req, res) => {
+    Foodie.find({}, (err, allFoodies)=>{
+        res.render('index.ejs', {
+            foodies: allFoodies
+        })
+    })
+})
+
+// new (create new restaurant and cuisine)
+router.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+// show (list of cuisines)
+router.get('/:id', (req,res)=>{
+    Foodie.findById(req.params.id, (err,foundFoodie)=>{
+        res.render('show.ejs', {
+        foodie: foundFoodie
+        })
+    })
+})
+
+// post 
+router.post('', (req,res)=>{
+    Foodie.create(req.body, (error, createdRestaurant) => {
+        res.redirect('/foodie')
+    })
+})
+
+// delete 
+router.delete('/:id', (req,res)=> {
+    Foodie.findByIdAndRemove(req.params.id, (err,data)=>{
+        res.redirect('/foodie')
+    })
+})
+
+// edit 
+router.get('/:id/edit', (req,res)=>{
+    Foodie.findById(req.params.id, (err, foundFoodie) => {
+        res.render('edit.ejs', {
+            foodie: foundFoodie,
+            id:req.params.id
+        })
+    })
+})
+
+// update
+router.put('/:id', (req, res) => {
+    Foodie.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err,updatedModel)=>{
+    res.redirect('/foodie')
+    })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const Foodie = require('./models/foodie.js')
 const methodOveride = require('method-override')
 const mongoose = require('mongoose')
+const foodieController = require('./controllers/foodie.js')
 
 mongoose.connect('mongodb://127.0.0.1:27017/foodie')
 mongoose.connection.once('open', ()=>{
@@ -16,61 +16,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(methodOveride('_method'))
 app.set('view engine', 'ejs')
-
-
-// index (list of restaurants)
-app.get('/foodie', (req, res) => {
-    Foodie.find({}, (err, allFoodies)=>{
-        res.render('index.ejs', {
-            foodies: allFoodies
-        })
-    })
-})
-
-// new restaurant
-app.get('/foodie/new', (req, res) => {
-    res.render('new.ejs')
-})
-
-// show (list of cuisines)
-app.get('/foodie/:id', (req,res)=>{
-    Foodie.findById(req.params.id, (err,foundFoodie)=>{
-        res.render('show.ejs', {
-        foodie: foundFoodie
-        })
-    })
-})
-
-// create a restaurant
-app.post('/foodie', (req,res)=>{
-    Foodie.create(req.body, (error, createdRestaurant) => {
-        res.redirect('/foodie')
-    })
-})
-
-// delete a restaurant
-app.delete('/foodie/:id', (req,res)=> {
-    Foodie.findByIdAndRemove(req.params.id, (err,data)=>{
-        res.redirect('/foodie')
-    })
-})
-
-// edit a restaurant
-app.get('/foodie/:id/edit', (req,res)=>{
-    Foodie.findById(req.params.id, (err, foundFoodie) => {
-        res.render('edit.ejs', {
-            foodie: foundFoodie,
-            id:req.params.id
-        })
-    })
-})
-
-// update
-app.put('/foodie/:id', (req, res) => {
-    Foodie.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err,updatedModel)=>{
-    res.redirect('/foodie')
-    })
-})
+app.use('/foodie', foodieController)
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000')
